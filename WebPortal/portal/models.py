024059d78af3bd32ad1f4c9dc53c12fff3561273
Louis Dang <ldanga98@gmail.com>
2019-11-14 00:16:17 -0600

    Added models and database migration

--- WebPortal/portal/models.py
+++ WebPortal/portal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+class Link(models.Model):
+    title=models.CharField(max_length=100)
+    url=models.URLField()
+    links=models.ManyToManyField(Category)
+
+    def __str__(self):
+        return self.title
+
+
