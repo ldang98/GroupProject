362ff2f32bb0c7eadd06be4939bce427e2f2c4bc
Mifa79 <anh.tx21@gmail.com>
2019-11-15 16:30:11 -0600

    Display CSS for Log in Page

--- WebPortal/WebPortal/settings.py
+++ WebPortal/WebPortal/settings.py
@@ -27,9 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-STATIC_URL = [
-    "/static/",
-]
 
 
 # Application definition
@@ -125,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ( os.path.join('static'), )
