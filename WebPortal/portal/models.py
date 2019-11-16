20642abe7240d0d7d0b6ff48f2e136725452346f
Louis Dang <ldanga98@gmail.com>
2019-11-15 22:57:09 -0600

    Switch User relationship from Category to Group model

--- WebPortal/portal/models.py
+++ WebPortal/portal/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import Group
 # Create your models here.
-class Category(models.Model):
-    name=models.CharField(max_length=100)
+# class Category(models.Model):
+#     name=models.CharField(max_length=100)
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
 class Link(models.Model):
     title=models.CharField(max_length=100)
     url=models.URLField()
-    links=models.ManyToManyField(Category)
+    links=models.ManyToManyField(Group)
 
     def __str__(self):
         return self.title
