63e1f40bd4657ee069bd84213eb868c4b1351f7f
Louis Dang <ldanga98@gmail.com>
2019-11-15 23:20:33 -0600

    deleted Category table and changed template folder

--- WebPortal/portal/admin.py
+++ WebPortal/portal/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Category, Link
+from .models import  Link
 
 # Register your models here.
-admin.site.register(Category)
+#admin.site.register(Category)
 admin.site.register(Link)
