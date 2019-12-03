c36d81119396286722243c878c7881e5c2ac30d5
Louis Dang <ldanga98@gmail.com>
2019-12-03 00:32:37 -0600

    new view function

--- WebPortal/portal/views.py
+++ WebPortal/portal/views.py
@@ -18,17 +18,18 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
+            if user.groups.exists():
+                    return redirect('account page')
+            # globalUrls = Link.objects.filter(links__name='Global')  # returns queryset using foreign key to where group is Global
+            # userGroup = user.groups.all()[0]  # object
 
-            globalUrls = Link.objects.filter(links__name='Global')  # returns queryset using foreign key to where group is Global
-            userGroup = user.groups.all()[0]  # object
-
-            if (userGroup.name != "Global"):
-                userUrls = Link.objects.filter(links__name=userGroup.name)  # queryset
-                links = list(chain(globalUrls, userUrls))  # concat querysets
-                context = {'links': links, 'Role': userGroup.name}
-            else:
-                context = {'links': globals}
-            return render(request, 'account.html', context)
+            # if (userGroup.name != "Global"):
+            #     userUrls = Link.objects.filter(links__name=userGroup.name)  # queryset
+            #     links = list(chain(globalUrls, userUrls))  # concat querysets
+            #     context = {'links': links, 'Role': userGroup.name}
+            # else:
+            #     context = {'links': globals}
+            # return render(request, 'account.html', context)
 
             # links = Link.objects.all()
             # category = user.get_group_permissions()
@@ -46,3 +47,16 @@ def login(request):
 
 def home(request):
     return render(request, 'home.html')
+
+def accountPage(request):
+    user=request.user
+    globalUrls = Link.objects.filter(links__name='Global')  # returns queryset using foreign key to where group is Global
+    userGroup = user.groups.all()[0]  # object
+
+    if (userGroup.name != "Global"):
+        userUrls = Link.objects.filter(links__name=userGroup.name)  # queryset
+        links = list(chain(globalUrls, userUrls))  # concat querysets
+        context = {'links': links, 'Role': userGroup.name}
+    else:
+        context = {'links': globalUrls,'Role':userGroup.name}
+    return render(request, 'account.html', context)
